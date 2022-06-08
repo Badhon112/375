64e839da01e13b959a72c0960bd3f979749a520e
Badhon <biswasbadhon896@gmail.com>
2022-06-09 05:54:26 +0600

    Unfinished work

---
+++ String/Medium/Wildcardstringmatching.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string wild,pattern;
+    getline(cin,wild);
+    getline(cin,pattern);
+    for(int i=0;i<pattern.size();i++){
+        for(int j=0;j<wild.size();j++){
+            
+        }
+    }
+
+}
